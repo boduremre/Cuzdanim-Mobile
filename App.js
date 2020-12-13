a2ca0223042e9329d1f18d8d4e8714f7a90ee316
boduremre <emrebodurbto@gmail.com>
2020-12-13 11:57:08 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+		<Text style={styles.textStyle}>Cüzdanım</Text>
+      <Text>Cüzdanım mobil uygulaması ile Gelir-Gider takibi yapabilirsiniz.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +18,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  }, textStyle: {
+    fontSize: 40,
   },
 });
